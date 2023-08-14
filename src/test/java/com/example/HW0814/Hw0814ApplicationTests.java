806a45be823dc63478b675c691263796cbeedc86
Tkakishak <tkakishakd@mail.ru>
2023-08-15 00:00:32 +0300

    Initial commit

---
+++ src/test/java/com/example/HW0814/Hw0814ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HW0814;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw0814ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
